0af8444d1c9fd5e4548563032b8e97a92d3e0493
thechocobowhisperer <the.chocobo.whisperer@gmail.com>
2019-05-04 10:58:55 -0500

    Moved guns information to state and edited functions to set values to state keys

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import './App.css';
 import {Button, Card, CardBody, CardTitle, CardText} from 'reactstrap';
 
 class App extends Component {
-  state = {}
+  state = {gunType:'', damageType:''}
 
   gunTypes = ['pistol', 'shotgun', 'rifle', 'smg']
   damageTypes = ['fire', 'poison', 'electric']
 
-  guns={gunType:'', damageType:''}
+  
 
   randomGunType() {
     let gun = this.gunTypes[Math.floor(Math.random() * (this.gunTypes.length))];
     console.log(gun)
-    this.guns.gunType =  gun
+    this.state.gunType = gun
   }
 
   randomDmgType() {
     let dmg = this.damageTypes[Math.floor(Math.random() * (this.damageTypes.length))];
     console.log(dmg)
-    this.guns.damageType = dmg
+    this.state.damageType = dmg
   }
 
   combineParts(event){
     this.randomGunType()
     this.randomDmgType()
-    console.log(this.guns)
-    return (this.guns)
+    console.log(this.state)
+    this.setState({state:event})
   }
   render () {
     
@@ -37,8 +37,8 @@ class App extends Component {
           <Card className='card'>
             <CardBody color='dark' >
               <CardTitle></CardTitle>
-              <CardText>Gun Type: {this.guns.gunType}</CardText>
-              <CardText>Damage Type: {this.guns.damageType}</CardText>
+              <CardText>Gun Type: {this.state.gunType}</CardText>
+              <CardText>Damage Type: {this.state.damageType}</CardText>
             </CardBody>
           </Card>
         </div>
